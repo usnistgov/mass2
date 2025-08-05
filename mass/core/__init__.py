35043fc814d4c5c268e35ad14baa4b69a88dbbd8
Joe Fowler <drjoefowler@gmail.com>
2025-08-05 12:54:30 -0600

    Remove tests that no longer make sense

--- mass/core/__init__.py
+++ mass/core/__init__.py
@@ -1,23 +1,11 @@
 # ruff: noqa: F403, F401
 
 import mass.core.analysis_algorithms
-import mass.core.channel_group
-import mass.core.channel_group_hdf5_only
-import mass.core.channel
-import mass.core.controller
-import mass.core.fake_data
-import mass.core.files
 import mass.core.optimal_filtering
 import mass.core.pulse_model
 
 
 from .analysis_algorithms import *
-from .channel_group import *
-from .channel_group_hdf5_only import *
-from .channel import *
-from .controller import *
-from .fake_data import *
-from .files import *
 from .optimal_filtering import *
 from .pulse_model import *
 from .projectors_script import make_projectors
@@ -25,9 +13,6 @@ from .experiment_state import ExperimentStateFile
 
 
 # Don't import the contents of these at the top level
-import mass.core.ljh_util
-import mass.core.ljh_modify
 import mass.core.message_logging
 import mass.core.utilities
 import mass.core.phase_correct
-import mass.core.ljh2off
