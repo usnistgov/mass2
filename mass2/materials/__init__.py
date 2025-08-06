12483f0b5f15984105d0ef4aa5dfcdd7c7deb468
Joe Fowler <drjoefowler@gmail.com>
2025-08-06 16:55:34 -0600

    More docstrings

--- mass2/materials/__init__.py
+++ mass2/materials/__init__.py
@@ -1,13 +1,6 @@
-try:
-    import xraydb
-
-except ModuleNotFoundError:
-    print('** Skipping module mass.materials, because it requires the "xraydb" python package.')
-    print('** Please see https://xraypy.github.io/XrayDB/installation.html for installation instructions.')
-
 from . import efficiency_models
 from .efficiency_models import FilterStack, Film, AlFilmWithOxide, AlFilmWithPolymer
 from .efficiency_models import LEX_HT, filterstack_models
 
-__all__ = ["xraydb", "efficiency_models", "FilterStack", "Film",
+__all__ = ["efficiency_models", "FilterStack", "Film",
            "AlFilmWithOxide", "AlFilmWithPolymer", "LEX_HT", "filterstack_models"]
--- mass2/materials/uncertainties_helpers.py
+++ mass2/materials/uncertainties_helpers.py
@@ -1,36 +1,85 @@
 import uncertainties
 from uncertainties import unumpy as unp
 import numpy as np
+from typing import Any
 
 
-def is_uncertain_scalar(x):
+def is_uncertain_scalar(x: Any) -> bool:
+    """Is x a value with uncertainties?
+
+    Parameters
+    ----------
+    x : Any
+        A scalar
+
+    Returns
+    -------
+    bool
+        Whether it has the attribute "nominal_value", and hence is an `uncertainties` scalar.
+    """
     return hasattr(x, "nominal_value")
 
 
-def ensure_uncertain(x):
-    """if give a scalar, returns a ufloat
-    if given a numpy array of scalars, return a uarray
-    if given a ufloat or uarray return it unchanged
-    default uncertainty will be 100%, so people will know not to take it seriously until they've
-    put it in manually
+def ensure_uncertain(x: Any) -> Any:
+    """Return the argument, in a form that has uncertainties.
+
+    * Given a scalar, returns a ufloat with 100% uncertainty
+    * Given a numpy array of scalars, return a uarray with 100% uncertainty
+    * Given a ufloat or uarray, return it unchanged
+
+    The default uncertainty will be 100%, so people will know not to take it seriously until they've
+    put it in manually. But if the argument is already uncertain, it will be returned unchanged.
+
+    Parameters
+    ----------
+    x : Any
+        The scalar or vector to be made uncertain
+
+    Returns
+    -------
+    Any
+        Either x itself, or an uncertain version of it (with ±100% uncertainty)
+
+    Raises
+    ------
+    ValueError
+        If type of `x` is unsupported
     """
     if isinstance(x, np.ndarray):
         if is_uncertain_scalar(x[0]):
             return x
-        else:
-            return unp.uarray(x, x)
-    elif isinstance(x, float):
+        return unp.uarray(x, x)
+    if isinstance(x, float):
         return uncertainties.ufloat(x, x)
-    elif is_uncertain_scalar(x):
+    if is_uncertain_scalar(x):
         return x
     else:
-        raise Exception(f"{x} of type {type(x)} not supported")
+        raise ValueError(f"{x} of type {type(x)} not supported")
 
 
-def with_fractional_uncertainty(x, fractional_uncertainty):
+def with_fractional_uncertainty(x: Any, fractional_uncertainty: float) -> Any:
+    """Return a version of x with the given fractional uncertainty
+
+    Parameters
+    ----------
+    x : Any
+        The scalar or vector to make uncertain
+    fractional_uncertainty : float
+        The relative uncertainty to impose
+
+    Returns
+    -------
+    Any
+        An uncertain version of x
+
+    Raises
+    ------
+    ValueError
+        If type of `x` is unsupported
+    """
     if isinstance(x, float):
         return uncertainties.ufloat(x, x * fractional_uncertainty)
-    elif isinstance(x, np.ndarray):
+    if isinstance(x, np.ndarray):
         return unp.uarray(x, x * fractional_uncertainty)
     else:
-        raise Exception(f"{x} of type {type(x)} not supported")
+        raise ValueError(f"{x} of type {type(x)} not supported")
