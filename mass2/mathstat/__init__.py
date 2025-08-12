f1a3aed348ae7d20f1b7fb3e515b088d465721ac
Joe Fowler <drjoefowler@gmail.com>
2025-08-12 15:55:12 -0600

    Fix name conflict

--- mass2/mathstat/__init__.py
+++ mass2/mathstat/__init__.py
@@ -5,6 +5,7 @@ Joe Fowler, NIST
 """
 
 from . import entropy, fitting, interpolate, power_spectrum, robust
-from . import special, toeplitz, utilities
+from . import special, toeplitz
+from . import utilities as mathutilities
 
-__all__ = ["entropy", "fitting", "interpolate", "power_spectrum", "robust", "special", "toeplitz", "utilities"]
+__all__ = ["entropy", "fitting", "interpolate", "power_spectrum", "robust", "special", "toeplitz", "mathutilities"]
