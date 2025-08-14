643343f53a6d03dd901fa511d650e02ed9c9d5c6
Joe Fowler <drjoefowler@gmail.com>
2025-08-14 16:10:20 -0600

    Automatically run code in docs/ markdown files
    This tests whether it runs w/o errors. It doesn't yet test for correct output. Relates to #17.

---
+++ tests/test_mkdocs.py
@@ -0,0 +1,20 @@
+import pathlib
+import pytest
+
+from mktestdocs import check_md_file
+
+"""test_mkdocs
+
+    Run tests in which we search for every markdown file in and under the `docs/` directory,
+    and check it for runnable python, and be sure that mktestdocs is happy.
+
+    See https://github.com/koaning/mktestdocs for where this script comes from.
+"""
+
+# Note the use of `str`, makes for pretty output
+
+
+@pytest.mark.parametrize("fpath", pathlib.Path("docs").glob("**/*.md"), ids=str)
+def test_files_good(fpath):
+    print(f"Testing {fpath}")
+    check_md_file(fpath=fpath, memory=True)
