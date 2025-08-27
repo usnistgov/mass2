85bbd590be07855f820eb9f93b4f4588a645f21b
Joe Fowler <drjoefowler@gmail.com>
2025-08-27 17:53:39 -0600

    Add type annotations
    Comment out some very old, broken "utilities"

--- mass2/core/utilities.py
+++ mass2/core/utilities.py
@@ -7,63 +7,68 @@ Various utility functions and classes:
     the terminal each time it yields.
 """
 
+from collections.abc import Callable
+from typing import Any
 import functools
 import time
 import sys
-import glob
-import os
-import subprocess
+
+# import glob
+# import os
+# import subprocess
 import logging
 import numpy as np
 import matplotlib.pylab as plt
+# from matplotlib.backend_bases import MouseEvent
+# from .channels import Channels
 
 import mass2
 
 
-class MouseClickReader:
-    """A callback for reading mouse clicks in data coordinates in pylab plots.
+# class MouseClickReader:
+#     """A callback for reading mouse clicks in data coordinates in pylab plots.
 
-    Stores self.b, .x, .y giving the button pressed, and the x,y data
-    coordinates of the pointer when clicked.
-    """
+#     Stores self.b, .x, .y giving the button pressed, and the x,y data
+#     coordinates of the pointer when clicked.
+#     """
 
-    def __init__(self, figure):
-        """Connect to button press events on a pylab figure.
+#     def __init__(self, figure: plt.Figure):
+#         """Connect to button press events on a pylab figure.
 
-        Args:
-            figure: The matplotlib.figure.Figure from which to capture mouse click events.
-        """
-        # The button number of the last mouse click inside a plot.
-        self.b = 0
-        # The x location of the last mouse click inside a plot.
-        self.x = 0
-        # The y location of the last mouse click inside a plot.
-        self.y = 0
-        # The Figure to whose events we are connected.
-        self.fig = figure
-        # The connection ID for matplotlib event handling.
-        self.cid = self.fig.canvas.mpl_connect("button_press_event", self)
+#         Args:
+#             figure: The matplotlib.figure.Figure from which to capture mouse click events.
+#         """
+#         # The button number of the last mouse click inside a plot.
+#         self.b = 0
+#         # The x location of the last mouse click inside a plot.
+#         self.x = 0
+#         # The y location of the last mouse click inside a plot.
+#         self.y = 0
+#         # The Figure to whose events we are connected.
+#         self.fig = figure
+#         # The connection ID for matplotlib event handling.
+#         self.cid = self.fig.canvas.mpl_connect("button_press_event", self)
 
-    def __call__(self, event):
-        """When called, capture the latest button number and the x,y location in
-        plot units.  Store in self.b, .x, and .y.
-        """
-        self.b, self.x, self.y = event.button, event.xdata, event.ydata
+#     def __call__(self, event: MouseEvent) -> None:
+#         """When called, capture the latest button number and the x,y location in
+#         plot units.  Store in self.b, .x, and .y.
+#         """
+#         self.b, self.x, self.y = event.button, event.xdata, event.ydata
 
-    def __del__(self):
-        """Disconnect the button press event from this object."""
-        self.fig.canvas.mpl_disconnect(self.cid)
+#     def __del__(self) -> None:
+#         """Disconnect the button press event from this object."""
+#         self.fig.canvas.mpl_disconnect(self.cid)
 
 
 class InlineUpdater:
-    def __init__(self, baseString):
+    def __init__(self, baseString: str):
         self.fracDone = 0.0
         self.minElapseTimeForCalc = 1.0
         self.startTime = time.time()
         self.baseString = baseString
         self.logger = logging.getLogger("mass")
 
-    def update(self, fracDone):
+    def update(self, fracDone: float) -> None:
         if self.logger.getEffectiveLevel() >= logging.WARNING:
             return
         self.fracDone = fracDone
@@ -73,7 +78,7 @@ class InlineUpdater:
             sys.stdout.write(f"\n{self.baseString} finished in {self.elapsedTimeStr}\n")
 
     @property
-    def timeRemaining(self):
+    def timeRemaining(self) -> float:
         if self.elapsedTimeSec > self.minElapseTimeForCalc and self.fracDone > 0:
             fracRemaining = 1 - self.fracDone
             rate = self.fracDone / self.elapsedTimeSec
@@ -85,7 +90,7 @@ class InlineUpdater:
             return -1
 
     @property
-    def timeRemainingStr(self):
+    def timeRemainingStr(self) -> str:
         timeRemaining = self.timeRemaining
         if timeRemaining == -1:
             return "?"
@@ -93,23 +98,23 @@ class InlineUpdater:
             return "%.1f min" % (timeRemaining / 60.0)
 
     @property
-    def elapsedTimeSec(self):
+    def elapsedTimeSec(self) -> float:
         return time.time() - self.startTime
 
     @property
-    def elapsedTimeStr(self):
+    def elapsedTimeStr(self) -> str:
         return "%.1f min" % (self.elapsedTimeSec / 60.0)
 
 
 class NullUpdater:
-    def update(self, f):
+    def update(self, f: float) -> None:
         pass
 
 
-def show_progress(name):
-    def decorator(func):
+def show_progress(name: str) -> Callable:
+    def decorator(func: Callable) -> Callable:
         @functools.wraps(func)
-        def work(self, *args, **kwargs):
+        def work(self: Any, *args: Any, **kwargs: Any) -> None:
             try:
                 if "sphinx" in sys.modules:  # supress output during doctests
                     print_updater = NullUpdater()
@@ -126,54 +131,57 @@ def show_progress(name):
     return decorator
 
 
-def plot_multipage(
-    data, subplot_shape, helper, filename_template_per_file, filename_template_glob, filename_one_file, format, one_file
-):
-    """Helper function for multipage printing. See plot_summary_pages() for an example of how to use it."""
+# def plot_multipage(
+#     data: Channels, subplot_shape: tuple[int, int], helper,
+#     filename_template_per_file, filename_template_glob, filename_one_file, format, one_file
+# ):
+#     """Helper function for multipage printing. See plot_summary_pages() for an example of how to use it."""
 
-    if format == "pdf" and one_file:
-        from matplotlib.backends.backend_pdf import PdfPages  # noqa: PLC0415
+#     if format == "pdf" and one_file:
+#         from matplotlib.backends.backend_pdf import PdfPages  # noqa: PLC0415
 
-        pdf = PdfPages(filename_one_file)
+#         pdf = PdfPages(filename_one_file)
 
-    (m, n) = subplot_shape
-    plt.clf()
-    for k, ds in enumerate(data.iter_channels()):
-        ax = plt.subplot(m, n, k % (m * n) + 1)
-        helper(ds, ax)
+#     (m, n) = subplot_shape
+#     plt.clf()
+#     for k, ds in enumerate(data.iter_channels()):
+#         ax = plt.subplot(m, n, k % (m * n) + 1)
+#         helper(ds, ax)
 
-        if ((k + 1) % (m * n)) == 0:
-            plt.tight_layout(True)
-            if format == "pdf" and one_file:
-                pdf.savefig()
-            else:
-                plt.savefig(filename_template_per_file % ((k + 1) // (m * n)))
-            plt.clf()
+#         if ((k + 1) % (m * n)) == 0:
+#             plt.tight_layout(True)
+#             if format == "pdf" and one_file:
+#                 pdf.savefig()
+#             else:
+#                 plt.savefig(filename_template_per_file % ((k + 1) // (m * n)))
+#             plt.clf()
 
-    # If final page is not full of plots, it hasn't yet been saved, so need to save it.
-    if (k + 1) % (m * n) != 0:
-        plt.tight_layout(True)
-        if format == "pdf" and one_file:
-            pdf.savefig()
-        else:
-            plt.savefig(filename_template_per_file % ((k + 1) // (m * n) + 1))
+#     # If final page is not full of plots, it hasn't yet been saved, so need to save it.
+#     if (k + 1) % (m * n) != 0:
+#         plt.tight_layout(True)
+#         if format == "pdf" and one_file:
+#             pdf.savefig()
+#         else:
+#             plt.savefig(filename_template_per_file % ((k + 1) // (m * n) + 1))
 
-    if format == "pdf" and one_file:
-        pdf.close()
+#     if format == "pdf" and one_file:
+#         pdf.close()
 
-    # Convert to a single file if requested by user
-    if format != "pdf" and one_file:
-        in_files = glob.glob(filename_template_glob)
-        if len(in_files) > 0:
-            in_files.sort()
-            cmd = ["convert"] + in_files + [filename_one_file]
-            ret = subprocess.call(cmd)
-            if ret == 0:
-                for f in in_files:
-                    os.remove(f)
+#     # Convert to a single file if requested by user
+#     if format != "pdf" and one_file:
+#         in_files = glob.glob(filename_template_glob)
+#         if len(in_files) > 0:
+#             in_files.sort()
+#             cmd = ["convert"] + in_files + [filename_one_file]
+#             ret = subprocess.call(cmd)
+#             if ret == 0:
+#                 for f in in_files:
+#                     os.remove(f)
 
 
-def annotate_lines(axis, label_lines, label_lines_color2=[], color1="k", color2="r"):
+def annotate_lines(
+    axis: plt.Axes, label_lines: list[str], label_lines_color2: list[str] = [], color1: str = "k", color2: str = "r"
+) -> None:
     """Annotate plot on axis with line names.
     label_lines -- eg ["MnKAlpha","TiKBeta"] list of keys of STANDARD_FEATURES
     label_lines_color2 -- optional,eg ["MnKAlpha","TiKBeta"] list of keys of STANDARD_FEATURES
