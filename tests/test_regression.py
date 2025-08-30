4238250a7c104fb04f89c06ba67b6378737948ca
Joe Fowler <drjoefowler@gmail.com>
2025-08-30 17:51:05 -0600

    Add a 2-channel regression test

---
+++ tests/test_regression.py
@@ -0,0 +1,58 @@
+import numpy as np
+import polars as pl
+import mass2
+import pulsedata
+
+
+def load_data():
+    pairs = pulsedata.pulse_noise_ljh_pairs["bessy_20240727"]
+    n = pairs.noise_folder
+    p = pairs.pulse_folder / ".." / "0001"
+    data = mass2.Channels.from_ljh_folder(p, n)
+    return data
+
+
+def all_steps(ch: mass2.Channel) -> mass2.Channel:
+    use_dc = pl.lit(True)
+    return (
+        ch.summarize_pulses()
+        .with_good_expr_pretrig_rms_and_postpeak_deriv(8, 8)
+        .filter5lag(f_3db=10000)
+        .driftcorrect(indicator_col="pretrig_mean", uncorrected_col="5lagy", use_expr=use_dc)
+    )
+
+
+def test_analysis_regression():
+    """Make sure that analysis results don't change without us noticing"""
+    data = load_data()
+    data = data.map(all_steps)
+    expected_df = pl.read_parquet("tests/regression_test_data.parquet")
+    for ch_num, ch in data.channels.items():
+        expect = expected_df.filter(pl.col("ch_num") == ch_num).drop("ch_num")
+        found = ch.df.drop("pulse", "timestamp", "subframecount")
+        print(expect)
+        print(found)
+        assert np.allclose(expect, found)
+
+
+###########################################################################
+# To update the regression_test_data.parquet file,
+# cd into this directory, tests/ under the main mass2 directory. Then:
+# python test_regression.py
+###########################################################################
+
+
+def store_analysis_regression():
+    """Use this to replace the test data file"""
+    data = load_data()
+    data = data.map(all_steps)
+    frames = []
+    for ch_num, ch in data.channels.items():
+        df = ch.df.drop("pulse", "timestamp", "subframecount").with_columns(ch_num=pl.lit(ch_num))
+        frames.append(df)
+    df = pl.concat(frames)
+    df.write_parquet("regression_test_data.parquet")
+
+
+if __name__ == "__main__":
+    store_analysis_regression()
