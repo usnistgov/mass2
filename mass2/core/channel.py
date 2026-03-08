2c10c8f14781ba5ee9e42308b62d283f0e5d875f
joefowler <1229009+joefowler@users.noreply.github.com>
2026-03-08 18:17:12 +0000

    automatic style fixes by ruff

--- mass2/core/channel.py
+++ mass2/core/channel.py
@@ -311,7 +311,8 @@ class Channel:
         if color_col is not None:
             columns_to_keep.append(color_col)
         df_small = (
-            self.df.lazy()
+            self.df
+            .lazy()
             .with_row_index(name=index_name)
             .filter(filter_expr)
             .select(*columns_to_keep)
@@ -861,7 +862,8 @@ class Channel:
             _description_
         """
         avg_pulse = (
-            self.df.lazy()
+            self.df
+            .lazy()
             .filter(self.good_expr)
             .filter(use_expr)
             .select(pulse_col)
@@ -950,7 +952,8 @@ class Channel:
             _description_
         """
         df = (
-            self.df.lazy()
+            self.df
+            .lazy()
             .filter(self.good_expr)
             .filter(use_expr)
             .limit(limit)
@@ -1154,7 +1157,8 @@ class Channel:
         assert off._mmap is not None
         df = pl.from_numpy(np.asarray(off._mmap))
         df = (
-            df.select(pl.from_epoch("unixnano", time_unit="ns").dt.cast_time_unit("us").alias("timestamp"))
+            df
+            .select(pl.from_epoch("unixnano", time_unit="ns").dt.cast_time_unit("us").alias("timestamp"))
             .with_columns(df)
             .select(pl.exclude("unixnano"))
         )
