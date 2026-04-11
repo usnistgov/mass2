321e33723f088a04bb4987ec7d0e53a062ac5306
Joe Fowler <drjoefowler@gmail.com>
2026-04-11 12:25:28 +0200

    First working version of mc2

--- mass2/core/mc2.py
+++ mass2/core/mc2.py
@@ -3,7 +3,25 @@ import glob
 import os
 from pathlib import Path
 import polars as pl
+import shutil
+import time
 import mass2
+
+
+def make_output(output_dir: Path, search_path: Path, force: bool):
+    if output_dir.exists() and len(glob.glob(str(search_path))) > 0:
+        if force:
+            newname = output_dir / ".." / "mc2_prev"
+            if newname.exists():
+                shutil.rmtree(newname)
+            os.rename(output_dir, newname)
+            output_dir.mkdir()
+        else:
+            raise OSError(
+                f"Cannot use existing output directory {output_dir} with parquet files, unless you choose the --force argument"
+            )
+    else:
+        output_dir.mkdir()
 
 
 def main() -> None:
@@ -19,6 +37,18 @@ def main() -> None:
         "-p", "--analysis-period", default=10.0, type=float,
         help="repeat basic analysis at this repetition period in seconds (default: 10)"
     )
+    parser.add_argument(
+        "-P", "--pulses_per_parquet", default=1000, type=int,
+        help="analyze no more that this many pulses per channel in each iteration (default: 1000)"
+    )
+    parser.add_argument(
+        "-m", "--minpulses", default=10, type=int,
+        help="don't analyze new data from a channel unless there are at least this many pulses (default: 10)"
+    )
+    parser.add_argument(
+        "-d", "--dry-run", action="store_true",
+        help="don't perform analysis, just say what would be done (default: False)"
+    )
     # parser.add_argument("-f", "--fast-analysis-period", default=1.0, type=float,
     #                     help="repeat basic analysis at this repetition period for any fast channels (seconds)")
     # parser.add_argument("-c", "--fast-channels", type=int, nargs="*", default=None,
@@ -27,28 +57,32 @@ def main() -> None:
     parser.add_argument("ljhpath", type=str, nargs=1, help="")
 
     args = parser.parse_args()
+    ljhpath = Path(args.ljhpath[0])
 
-    ljhpath = Path(args.ljhpath)
-    data = mass2.core.Channels.from_ljh_folder(ljhpath)
+    output_dir = args.output_dir
+    if output_dir is None:
+        output_dir = ljhpath / "mc2"
+    if not args.dry_run:
+        search_path = output_dir / "*_analyzed_*.parquet"
+        make_output(output_dir, search_path, force=args.force)
 
-    output_dir = ljhpath / "mc2"
-    filefullpath = data.ch0.header.df["Filename"][0]
+    paths = mass2.core.ljhutil.find_ljh_files(ljhpath)
+    paths = mass2.core.ljhutil.ljh_sort_filenames_numerically(paths)
+    filefullpath = paths[0]
     _, filename = os.path.split(filefullpath)
     prefix = filename.split("_chan")[0]
     parquet_file_prefix = str(output_dir / f"{prefix}_analyzed_")
-    if output_dir.exists():
-        search_path = output_dir / "*_analyzed_*.parquet"
-        if args.force:
-            os.rename(output_dir, ljhpath / "mc2_prev")
-        elif len(glob.glob(str(search_path))) > 0:
-            raise OSError(
-                f"Cannot use existing output directory {output_dir} with parquet files, unless you choose the --force argument"
-            )
 
-    else:
-        output_dir.mkdir()
+    ljhfiles = {}
+    for p in paths:
+        ljh = mass2.core.LJHFile.open(p, last_pulse=args.pulses_per_parquet)
+        ljhfiles[ljh.channum] = ljh
+    del ljhfiles[0]
 
-    run_recipe_loop(data, args.recipefile, parquet_file_prefix, args.verbose)
+    try:
+        run_recipe_loop(ljhfiles, parquet_file_prefix, args)
+    except KeyboardInterrupt:
+        return
 
 
 ENCODING = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
@@ -65,19 +99,50 @@ def encode_counter(j: int, nchar: int = 4) -> str:
     return "".join(c)
 
 
-def run_recipe_loop(data: mass2.Channels, recipefile: str, parquet_file_prefix: str, verbose: bool = False) -> None:
-    parquet_counter = 0
-    while True:
-        code = encode_counter(parquet_counter)
-        parquet_file = str(parquet_file_prefix) + f"{code}.parquet"
-        data = data.load_recipes(recipefile)
-        df = pl.DataFrame()
-        for ch_num, ch in data.channels.items():
-            chdf = ch.df.drop("pulse").with_columns(channel=pl.lit(ch_num))
-            df = pl.concat([df, chdf])
-        df.write_parquet(parquet_file)
-        parquet_counter += 1
+def run_recipe_loop(ljhfiles: dict[int, mass2.LJHFile], parquet_file_prefix: str, args : argparse.Namespace) -> None:
 
-        break
-        # TODO: wipe old info from each channel in data, add any incremental data newly added to the LJH file.
-        # For a quick test, we could maybe just read LJH files 1000 pulses at a time.
+    recipefile = args.recipefile[0]
+    analysis_period = args.analysis_period
+    print(f"Analysis period: {analysis_period} seconds")
+    # verbose = args.verbose
+    parquet_counter = 0
+    processed_counter = {ch_num: 0 for ch_num in ljhfiles.keys()}
+
+    while True:
+        channels = {ch_num: mass2.Channel.from_open_ljh(ljh) for (ch_num, ljh) in ljhfiles.items()}
+        for ch_num in ljhfiles.keys():
+            if channels[ch_num].npulses <= 0:
+                del channels[ch_num]
+        data = mass2.Channels(channels, "mc2 channels")
+        if not args.dry_run:
+            data = data.load_recipes(recipefile)
+        dframes = []
+        # processed = set({})
+        for ch_num, ch in data.channels.items():
+            if len(ch.df) < args.minpulses:
+                continue
+            # processed.add(ch_num)
+            chdf = ch.df.drop("pulse").with_columns(channel=pl.lit(ch_num))
+            processed_counter[ch_num] += len(chdf)
+            print(f"Channel {ch_num:3d} processed {len(chdf):5d} pulses.")
+            dframes.append(chdf)
+
+        # Write parquet file, if there are any data to write.
+        if len(dframes) > 0:
+            df = pl.concat(dframes)
+            code = encode_counter(parquet_counter)
+            parquet_file = str(parquet_file_prefix) + f"{code}.parquet"
+            if not args.dry_run:
+                df.write_parquet(parquet_file)
+            parquet_counter += 1
+            print(f"...written to {parquet_file}\n")
+        else:
+            print("...waiting for new data")
+            time.sleep(5.0)
+
+        # Now wait a time to load and analyze new data.
+        time.sleep(analysis_period)
+        for ch_num in data.channels.keys():
+            first = processed_counter[ch_num]
+            last = first + args.pulses_per_parquet
+            ljhfiles[ch_num] = ljhfiles[ch_num].reopen_binary(first, last)
