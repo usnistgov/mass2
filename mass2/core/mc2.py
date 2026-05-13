a0eaec40248f3fcf9b8a8bbbadff4a67969b9f5d
Joe Fowler <drjoefowler@gmail.com>
2026-05-13 11:49:17 -0600

    Formatting updates

--- mass2/core/mc2.py
+++ mass2/core/mc2.py
@@ -35,20 +35,28 @@ def main() -> None:
         "-o", "--output-dir", nargs="?", const=None, default=None, help="store output to this directory (default: $ljhpath/mc2)"
     )
     parser.add_argument(
-        "-p", "--analysis-period", default=10.0, type=float,
-        help="repeat basic analysis at this repetition period in seconds (default: 10)"
+        "-p",
+        "--analysis-period",
+        default=10.0,
+        type=float,
+        help="repeat basic analysis at this repetition period in seconds (default: 10)",
     )
     parser.add_argument(
-        "-P", "--pulses_per_parquet", default=1000, type=int,
-        help="analyze no more that this many pulses per channel in each iteration (default: 1000)"
+        "-P",
+        "--pulses_per_parquet",
+        default=1000,
+        type=int,
+        help="analyze no more that this many pulses per channel in each iteration (default: 1000)",
     )
     parser.add_argument(
-        "-m", "--minpulses", default=10, type=int,
-        help="don't analyze new data from a channel unless there are at least this many pulses (default: 10)"
+        "-m",
+        "--minpulses",
+        default=10,
+        type=int,
+        help="don't analyze new data from a channel unless there are at least this many pulses (default: 10)",
     )
     parser.add_argument(
-        "-d", "--dry-run", action="store_true",
-        help="don't perform analysis, just say what would be done (default: False)"
+        "-d", "--dry-run", action="store_true", help="don't perform analysis, just say what would be done (default: False)"
     )
     # parser.add_argument("-f", "--fast-analysis-period", default=1.0, type=float,
     #                     help="repeat basic analysis at this repetition period for any fast channels (seconds)")
@@ -93,7 +101,8 @@ ENCODING = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
 
 def encode_counter(j: int, nchar: int = 4) -> str:
     """Encode integer `j` as a base-32 string, using the 32 characters in ENCODING
-    (Crockford's base-32 encoding)."""
+    (Crockford's base-32 encoding: https://www.crockford.com/base32.html). Use this
+    for labeling Parquet files. 4 characters encodes 20 bits, or over 1 million values."""
     c = []
     for i in range(nchar):
         c.append(ENCODING[j % 32])
@@ -103,7 +112,7 @@ def encode_counter(j: int, nchar: int = 4) -> str:
 
 
 def last_good_expr(ch: mass2.Channel) -> pl.Expr:
-    "Use the `good_expr` from the last step that has one, or literal True if none do."
+    """Use the `good_expr` from the last step that has one, or literal True if none do."""
     for step in reversed(ch.steps):
         try:
             return step.good_expr
@@ -113,7 +122,6 @@ def last_good_expr(ch: mass2.Channel) -> pl.Expr:
 
 
 def run_recipe_loop(ljhfiles: dict[int, mass2.LJHFile], parquet_file_prefix: str, args: argparse.Namespace) -> None:
-
     recipefile = args.recipefile[0]
     analysis_period = args.analysis_period
     # verbose = args.verbose
@@ -135,11 +143,11 @@ def run_recipe_loop(ljhfiles: dict[int, mass2.LJHFile], parquet_file_prefix: str
                 continue
             # processed.add(ch_num)
             good_expr = last_good_expr(ch)
-            chdf = (ch.df
-                    .drop("pulse")
-                    .with_row_index(name="pulseindex")
-                    .with_columns(good_expr.alias("good"), pl.lit(ch_num).alias("channel"))
-                    )
+            chdf = (
+                ch.df.drop("pulse")
+                .with_row_index(name="pulseindex")
+                .with_columns(good_expr.alias("good"), pl.lit(ch_num).alias("channel"))
+            )
             processed_counter[ch_num] += len(chdf)
             print(f"Channel {ch_num:3d} processed {len(chdf):5d} pulses.")
             dframes.append(chdf)
