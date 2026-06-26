fd414b940d6fc1ccd7745b722194d70e0cdc04cf
Joe Fowler <drjoefowler@gmail.com>
2026-06-26 12:13:33 -0600

    Simplify the inverted data test
    (to make it compatible with no raw data in the polars DataFrame)

--- tests/core/test_transformed_raw.py
+++ tests/core/test_transformed_raw.py
@@ -1,6 +1,4 @@
 import numpy as np
-import polars as pl
-from polars.testing import assert_frame_equal
 import pulsedata
 import mass2
 
@@ -16,40 +14,13 @@ def test_inverted_data():
 
     ds1 = mass2.Channel.from_ljh(src_name)
     ds2 = mass2.Channel.from_ljh(src_name, transform_raw=invert)
-    raw1 = ds1.df["pulse"].to_numpy()
-    raw2 = ds2.df["pulse"].to_numpy()
+    all = range(0, ds1.npulses)
+    assert ds1.pulsereader is not None
+    assert ds2.pulsereader is not None
+    raw1 = ds1.load_raw(all)
+    raw2 = ds2.load_raw(all)
+    assert ds2.transform_raw is not None
     tr2 = ds2.transform_raw(raw2)
 
-    assert np.all(raw1 == raw2)
-    assert np.all(raw1 == ~tr2)
-
-    src_name = pulsedata.pulse_noise_ljh_pairs["bessy_20240727"].pulse_folder / "20240727_run0002_chan4219.ljh"
-
-    ds1 = mass2.Channel.from_ljh(src_name)
-    ds2 = mass2.Channel.from_ljh(src_name, transform_raw=invert)
-
-    # Use only a limited # of rows for this test
-    nmax = 400
-    ds1 = ds1.with_replacement_df(ds1.df.limit(nmax))
-    ds2 = ds2.with_replacement_df(ds2.df.limit(nmax))
-
-    # Now replace ds2's raw data with a bitwise inverse of it
-    inverted = invert(ds2.df["pulse"].to_numpy())
-    df2 = ds2.df.drop("pulse").with_columns(pl.Series(inverted).alias("pulse"))
-    ds2 = ds2.with_replacement_df(df2)
-
-    # Make sure the pulse data are indeed inverted
-    raw1 = ds1.df["pulse"].to_numpy()
-    raw2 = ds2.df["pulse"].to_numpy()
-    tr2 = ds2.transform_raw(raw2)
     assert np.all(raw1 == ~raw2)
     assert np.all(raw1 == tr2)
-
-    # Now remove the raw pulse column and compare all other columns in the data frame
-    ds1 = ds1.summarize_pulses()
-    ds2 = ds2.summarize_pulses()
-    df1 = ds1.df.drop("pulse")
-    df2 = ds2.df.drop("pulse")
-    print(df1.limit(5))
-    print(df2.limit(5))
-    assert_frame_equal(df1, df2, check_exact=False)
