ff4f59c62ae9fb94e34b6e95075b24a036599952
Joe Fowler <drjoefowler@gmail.com>
2026-08-22 15:50:25 -0600

    Try a polars-specific comparison for regressions

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,7 +1,7 @@
-import numpy as np
 import polars as pl
 import mass2
 import pulsedata
+from polars.testing import assert_frame_equal
 
 
 def load_data():
@@ -32,7 +32,7 @@ def test_analysis_regression():
     for ch_num, ch in data.channels.items():
         expect = expected_df.filter(pl.col("ch_num") == ch_num).drop("ch_num")
         found = ch.df.drop("pulse", "timestamp", "subframecount", strict=False)
-        assert np.allclose(expect, found, rtol=3e-5, atol=1e-6)
+        assert_frame_equal(expect, found, rel_tol=3e-5, abs_tol=1e-6, check_exact=False)
 
 
 ###########################################################################
